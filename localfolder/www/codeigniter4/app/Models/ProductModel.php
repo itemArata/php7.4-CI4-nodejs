a596299b79b3116dce27dcaebc596cf962df62e4
nwb43 <arata.hatori@i-tem.info>
2021-10-20 19:41:03 +0900

    modified config but failed to get json

---
+++ localfolder/www/codeigniter4/app/Models/ProductModel.php
@@ -0,0 +1,20 @@
+//CRUD操作を実装するために、データベースアクティビティを実行するためのモデルクラス をここに書いておく
+
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class ProductModel extends Model {
+
+	protected $table      = 'product';
+    protected $primaryKey = 'id';
+	
+	protected $returnType     = 'array';
+
+    protected $allowedFields = ['name', 'price', 'sale_price', 'sales_count', 'sale_date'];
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+	
+}
